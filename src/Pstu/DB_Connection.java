57594428d3444b977672f2dfc82037e88de7054c
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2022-01-12 00:00:31 +0600

    Check Database Connection

--- src/Pstu/DB_Connection.java
+++ src/Pstu/DB_Connection.java
@@ -4,11 +4,28 @@
  * and open the template in the editor.
  */
 package Pstu;
+import java.sql.*;  
 
 /**
  *
  * @author ASUS
  */
 public class DB_Connection {
+    Connection c;
+    Statement s;
     
+    public static void main(String[] args){
+         new DB_Connection();
+    }
+     
+     
+    public DB_Connection(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");
+           Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/java","root","");   
+            System.out.println("Alhamdulillah");
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    } 
 }
